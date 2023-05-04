c67f4ca16bc321df460f2e7d7dc1a31f6f41cba4
sagarbharvadiya <sagarbharvadiya@gmail.com>
2023-05-04 20:06:28 +0530

    solution detials

---
+++ src/components/CollaborationArchitecture.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import dataJson from '../Data/data.json';
+
+const CollaborationArchitecture = () => {
+  return (
+    <>
+      <div className='wrapper'>
+        <div className='box'>
+          {dataJson['Collaboration Architecture'].map((d, i) => {
+            return (
+              <React.Fragment key={d.id}>
+                <div className="d-flex">
+                  <p>0{d.id}</p>
+                  <h2>{d.title}</h2>
+                </div>
+                <ul>
+                  {d.link1 && <li>{d.link1}</li>}
+                  {d.link2 && <li>{d.link2}</li>}
+                  {d.link3 && <li>{d.link3}</li>}
+                  {d.link4 && <li>{d.link4}</li>}
+                  {d.link5 && <li>{d.link5}</li>}
+                  {d.link6 && <li>{d.link6}</li>}
+                </ul>
+              </React.Fragment>
+            );
+          })}
+        </div>
+        <div className="image">
+          {dataJson.img2.map((d, i) => {
+            return <img key={i} src={d.img} alt='' />;
+          })}
+        </div>
+        <div className="dec">
+          {dataJson['Solution Offering'].map((d, i) => {
+            return (
+              <React.Fragment key={i}>
+                <h2>{d.title}</h2>
+                <p>{d.desc}</p>
+                <div className='btn'>
+                  <button>{d.btn}</button>
+                </div>
+              </React.Fragment>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CollaborationArchitecture;
