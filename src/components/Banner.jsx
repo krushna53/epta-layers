c463627b231db550cdedeada564bbd6d28e8cd82
Himmatlal Parmar <himmatlalparmar785@gmail.com>
2023-05-11 17:53:54 +0530

    chnages in responsive

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import banner_image from "../images/banner-image.png";
 import banner_des_image from '../images/logo-image.png';
+import banner_mobile_image from '../images/banner-mobile-image.jpg';
 import Aos from 'aos';
 
 const Banner = () => {
@@ -14,6 +15,9 @@ const Banner = () => {
                 <div className='banner-container'>
                     <div className='banner-image'>
                         <img src={banner_image}/>
+                    </div>
+                    <div className='banner-mobile-image'>
+                        <img src={banner_mobile_image}/>
                     </div>
                     <div className='banner-des-folder'>
                         <div className='banner-des-image' data-aos="fade-down">
