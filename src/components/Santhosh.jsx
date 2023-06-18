33c189be73717dd9e872afdc1adec13ae323ef7f
sagarbharvadiya <sagarbharvadiya@gmail.com>
2023-06-18 08:48:40 +0530

    some-changes

--- src/components/Santhosh.jsx
+++ src/components/Santhosh.jsx
@@ -8,20 +8,19 @@ const Santhosh = () => {
           <div className="santos_left_img">
             <img src="../images/Santhosh photo v2 (1).png" alt="santhosh" />
           </div>
-          
+
           <div className="santos_right_con">
-            <h3>Santhosh Baragur</h3>
+            <h2>Santhosh Baragur</h2>
             <p>
-              After spending more than 20 years in IT Sales and Business
-              Development for Enterprise Hardware & Software segment, I decided
-              to start up Epta Layers Pvt. Ltd. where I drive business
-              continuity and Enterprise IT transformation.
+              After spending more than 20 years in Sales and Business
+              Development for the Enterprise IT segment, Santhosh launched Epta
+              Layers Pvt. Ltd. where he drives business continuity and
+              Enterprise IT transformation.
             </p>
             <p>
-              My keen sense of identifying prospective customers and
-              establishing long-term business relationships gives me a strong
-              drive for business excellence without losing sight of top and
-              bottom-line profitability.
+              His keen sense of identifying prospective customers gives Santhosh
+              a strong drive for business excellence without losing sight of the
+              top and bottom-line profitability.
             </p>
             <button>
               <a href="/">Connect on LinkedIn</a>
