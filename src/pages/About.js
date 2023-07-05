e81d8b5e5b5353e5ca82644c944b9085b852aa55
sagarbharvadiya <sagarbharvadiya@gmail.com>
2023-07-05 18:33:31 +0530

    clean

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,6 +6,7 @@ import OurGuiding from '../components/OurGuiding'
 import Santhosh from '../components/Santhosh'
 import WeAre from '../components/WeAre'
 import Footer from '../components/Footer'
+import { ToastContainer } from "react-toastify";
 const About = () => {
 
     return (
@@ -21,7 +22,18 @@ const About = () => {
                 rel="stylesheet"
                 href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
             />
-
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="dark"
+            />
         </>
     )
 }
