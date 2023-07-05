d5ccff273a4deabcb124d4bdb113a67497aa8af2
sagarbharvadiya <sagarbharvadiya@gmail.com>
2023-07-05 20:58:12 +0530

    responsive fix

--- src/components/OurGuiding.jsx
+++ src/components/OurGuiding.jsx
@@ -1,46 +1,50 @@
-import React, { useEffect } from 'react'
-
+import React, { useEffect } from "react"
 
 const OurGuiding = () => {
-
   return (
     <>
-    <div className='main'>
-    <div className='bg_ourGuiding'>
-      <section className="OurGuiding">
-        <div className="wrapper">
-        <div className="OurGuiding_wrapper">
-        <h2>Our Guiding Principles</h2>
-          <div className="d-flex">
-            <div className="left_box">
-              <img src="../images/14.png" alt="" />
-              <div className="con">
-                <h5>Vision</h5>
-                <p>
-                  To provide the best IT infrastructure solutions and help our
-                  customers improve their business continuity. We aim to become
-                  the most reliable technology partner to enterprise customers.
-                </p>
+      <div className="main">
+        <div className="bg_ourGuiding">
+          <section className="OurGuiding">
+            <div className="wrapper">
+              <div className="OurGuiding_wrapper">
+                <h2>Our Guiding Principles</h2>
+                <div className="d-flex">
+                  <div className="left_box">
+                    {/* <img src="../images/14.png" alt="" /> */}
+                    <div className="con">
+                      <div className="box_con">
+                        <h5>Vision</h5>
+                        <p>
+                          To provide the best IT infrastructure solutions and
+                          help our customers improve their business continuity.
+                          We aim to become the most reliable technology partner
+                          to enterprise customers.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="right_box">
+                    {/* <img src="../images/14.png" alt="" /> */}
+                    <div className="con">
+                      <div className="box_con">
+                        <h5>Mission</h5>
+                        <p>
+                          To be the point solutions team for all IT
+                          infrastructure needs. From implementation to business
+                          continuity, we are dedicated to delivering the best
+                          possible customer experience and strive to continually
+                          improve our solution offerings.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
-            <div className="right_box">
-            <img src="../images/14.png" alt="" />
-              <div className="con">
-                <h5>Mission</h5>
-                <p>
-                  To be the point solutions team for all IT infrastructure
-                  needs. From implementation to business continuity, we are
-                  dedicated to delivering the best possible customer experience
-                  and strive to continually improve our solution offerings.
-                </p>
-              </div>
-            </div>
-          </div>
+          </section>
         </div>
-        </div>
-      </section>
-    </div>
-    </div>
+      </div>
     </>
   )
 }
