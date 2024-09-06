f5501434bbcfdf96809eab9616068bdbc1a0554d
vinodkeshariya <keshariyavinod793@gmail.com>
2024-09-06 20:13:58 +0530

    update case-study 2

--- src/components/CaseStudiesDetails.jsx
+++ src/components/CaseStudiesDetails.jsx
@@ -1,6 +1,8 @@
 import React from "react"
 import kv2 from "../images/kv2.jpg"
 import Casestudy1pdf from "../pdf/Epta-CaseStudy-1-IT-ITeS.pdf"
+import banner2 from "../images/EptaCaseStudy-2-Banner.png"
+import Casestudy2pdf from "../pdf/Epta-CaseStudy-2-IT-Transformation.pdf"
 
 const CaseStudiesDetails = () => {
   return (
@@ -21,24 +23,24 @@ const CaseStudiesDetails = () => {
               </div>
             </div>
           </div>
-          {/* <div className="vc-line">
+          <div className="vc-line">
             <span></span>
           </div>
           <div className="case_studies">
             <div className="case_left">
-              <h1></h1>
+              <h1>From Complexity to Clarity: Epta's IT Transformation for Engineering Excellence</h1>
               <p>
-                 
+              Discover how Epta Layers revolutionized the IT infrastructure architecture of a global engineering company, enhancing project efficiency and scalability through tailored solutions in IT infrastructure. 
               </p>
               <div className="btn">
-                <a href="">Click here more details</a>
+                <a href={Casestudy2pdf} target="_blank">Click here more details</a>
               </div>
             </div>
             <div className="case_right">
-              <img src={kv2} alt="case" />
+              <img src={banner2} alt="case" />
             </div>
           </div>
-          <div className="vc-line">
+          {/* <div className="vc-line">
             <span></span>
           </div>
           <div className="case_studies">
